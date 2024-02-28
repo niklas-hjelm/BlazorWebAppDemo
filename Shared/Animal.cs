748e035324881568e3e6762921dd20f0889edce8
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-28 10:16:16 +0100

    update

--- Shared/Animal.cs
+++ Shared/Animal.cs
@@ -1,7 +1,16 @@
-﻿namespace Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared;
 
 public class Animal
 {
+    [StringLength(10, MinimumLength = 3, ErrorMessage = "Namnet måste vara mellan 3 och 10 tecken långt!")]
+    [Required(ErrorMessage = "Fyll i namnet bror!")]
     public string Name { get; set; }
+    [Required]
     public string Type { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    [Phone]
+    public string Phone { get; set; }
 }
